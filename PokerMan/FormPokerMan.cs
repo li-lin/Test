1c71f25e80286c7d048e801845a71b67836f9138
li-lin <lin.lee@live.cn>
2016-04-02 14:42:04 +0800

    created pokerman project

---
+++ PokerMan/FormPokerMan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PokerMan
+{
+    public partial class FormPokerMan : Form
+    {
+        public FormPokerMan()
+        {
+            InitializeComponent();
+        }
+    }
+}
