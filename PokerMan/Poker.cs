8870d86825be7496dc0681403d7c3e0dad9219f8
li-lin <linlee@foxmail.com>
2016-04-02 16:14:11 +0800

    created poker class , suit enum and value enum.

---
+++ PokerMan/Poker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerMan
+{
+    public class Poker
+    {
+        public Suit Suit { get; private set; }
+        public Value Value { get; private set; }
+        public Poker(Suit suit, Value value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}-{1}", this.Suit, this.Value.ToString().Substring(1));
+        }
+    }
+}
---
+++ PokerMan/SuitsAndValues.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokerMan
+{
+    public enum Suit
+    {        
+        Spade, //黑桃
+        Heart, //红桃
+        Club, //梅花
+        Diamond //方块
+    }
+
+    public enum Value
+    {
+        A,
+        K,
+        Q,
+        J,
+        _10,
+        _9,
+        _8,
+        _7,
+        _6,
+        _5,
+        _4,
+        _3,
+        _2
+    }
+}
